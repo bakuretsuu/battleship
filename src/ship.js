e6dfe1180aa32f849c01a2adca0723193dea2bc6
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-11-23 09:14:10 +0800

    updated test cases for ship and created initial ship class

--- src/ship.js
+++ src/ship.js
@@ -1,1 +1,18 @@
-export { ship }
+export { Ship }
+
+class Ship {
+    constructor(length){
+        this.length = length;
+        this.hits = 0;
+    }
+    hit(){
+        this.hits++;
+    }
+    isSunk(){
+       if(this.length <= this.hits){
+        return true;
+       } else {
+        return false;
+       }
+    }
+}
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,20 +1,8 @@
-import {ship} from "./src/ship"
+import {Ship} from "./src/ship"
 
 test('ship has correct length', () => {
     const ship = new Ship(3);
     expect(ship.length).toBe(3);
-});
-
-test('ship is not sunk when hits < length', () => {
-    const ship = new Ship(3);
-    ship.hits = 2; 
-    expect(ship.isSunk()).toBeFalsy();
-});
-
-test('ship is sunk when hits >= length', () => {
-    const ship = new Ship(3);
-    ship.hits = 3; 
-    expect(ship.isSunk()).toBeTruthy();
 });
 
 test('ship has correct number of times hit', () => {
@@ -22,4 +10,21 @@ test('ship has correct number of times hit', () => {
     ship.hit();
     ship.hit();
     expect(ship.hits).toBe(2);
+})
+
+test('ship is not sunk when hits < length', () => {
+    const ship = new Ship(3);
+    ship.hit(); 
+    ship.hit(); 
+    expect(ship.isSunk()).toBeFalsy();
 });
+
+test('ship is sunk when hits >= length', () => {
+    const ship = new Ship(3);
+    ship.hit(); 
+    ship.hit(); 
+    ship.hit(); 
+    expect(ship.isSunk()).toBeTruthy();
+});
+
+;
