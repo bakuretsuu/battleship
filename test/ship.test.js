ebcf0468a13e00f5be4d31a35af02d531135b4bb
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-11-23 09:03:43 +0800

    create test cases for ship class

---
+++ test/ship.test.js
@@ -0,0 +1,25 @@
+import {ship} from "./src/ship"
+
+test('ship has correct length', () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+});
+
+test('ship is not sunk when hits < length', () => {
+    const ship = new Ship(3);
+    ship.hits = 2; 
+    expect(ship.isSunk()).toBeFalsy();
+});
+
+test('ship is sunk when hits >= length', () => {
+    const ship = new Ship(3);
+    ship.hits = 3; 
+    expect(ship.isSunk()).toBeTruthy();
+});
+
+test('ship has correct number of times hit', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    expect(ship.hits).toBe(2);
+});
