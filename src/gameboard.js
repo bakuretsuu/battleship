e265d0cdc1ac3090e41517d318073b52a8ec01de
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-11-25 07:46:52 +0800

    added grid constructor

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,11 @@
 export class Gameboard {
-    
+    constructor(){
+        this.grid = [];
+        for(let i = 0; i < 10; i++){
+            this.grid[i] = [];   
+            for(let j = 0; j < 10; j++){
+                this.grid[i][j] = 0;
+            }
+        }
+    }
 }
