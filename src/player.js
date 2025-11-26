5ba0543fccccfbb914dd0675ae754681154cf46d
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-11-27 05:59:17 +0800

    create and test  player class

--- src/player.js
+++ src/player.js
@@ -2,5 +2,9 @@ import { Gameboard } from "../src/gameboard.js";
 import { Ship } from "../src/ship.js";
 
 export class Player {
+    constructor(type = 'human'){
+        this.type = type;
+        this.gameboard = new Gameboard();
+    }
     
 }
